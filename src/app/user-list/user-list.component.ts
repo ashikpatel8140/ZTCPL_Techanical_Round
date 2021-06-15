0d5dda5420315620b5f0457d61c9af13160bb6fe
ashikpatel8140 <66195857+ashikpatel8140@users.noreply.github.com>
2021-06-15 20:14:43 +0530

    initial-change

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -11,47 +11,31 @@ import { UserService } from '../user.service';
   styleUrls: ['./user-list.component.css']
 })
 export class UserListComponent implements OnInit {
-     users : {id:number,first_name:string,last_name:string,email:string,phone:number}[] = [
-        
-    ];
-    isUser = false;
-    
-    modalRef!: BsModalRef;
-    constructor(private modalService: BsModalService,private userService:UserService) {}
-   
-    openModal(template: TemplateRef<any>) {
-      this.modalRef = this.modalService.show(template);
-    }
-    generateId(){
-      return ;
-    }
+  users : {id:number,first_name:string,last_name:string,email:string,phone:number}[] = [];
 
-    onSubmit(form:NgForm){
+  modalRef!: BsModalRef;
+  constructor(private modalService: BsModalService,private userService:UserService) {}
+  ngOnInit(): void {
+    this.loadData();
+  } 
+  
+  openModal(template: TemplateRef<any>) { this.modalRef = this.modalService.show(template);}
+   
+  onSubmit(form:NgForm){
       if(form.valid){
         const obj = {id:new Date().getMilliseconds(),first_name:form.value.first_name,last_name:form.value.last_name,email:form.value.email,phone:form.value.phone} 
         this.userService.users.push(obj);  
         this.userService.addUser(this.userService.users);
         this.modalRef.hide();
-        this.isUser = true;
       }
     }
     
-    onDelete(id:number){
-        // this.userService.removeUser(id);
-
-        this.userService.users =  this.userService.users.filter(u=>{
-          return u.id != id
-        })
+  onDelete(id:number){
+        this.userService.users =  this.userService.users.filter(u=>u.id != id);
         localStorage.removeItem('users');
-        localStorage.setItem('users',JSON.stringify(this.users));
+        localStorage.setItem('users',JSON.stringify(this.userService.users));
         this.loadData();
     }
-  
-
-  ngOnInit(): void {
-    this.loadData();
-  }
-
   loadData(): void{
     this.users = this.userService.users;
   }
